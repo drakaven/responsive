6f7b1fda1ccc821a77808edc5e4a11cd76371460
drakaven <drakaven@hotmail.com>
2017-03-14 18:46:16 -0400

    added jquery functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("Loaded");
+$(document).ready(function () {
+
+  $('#form-submit').on('click', function (event) {
+    if ($('#telephone-input')[0].value === '') {
+      event.preventDefault();
+    }
+  });
+
+  $('#business-card-checkbox').on('change', function () {
+    $('#business-card-section').toggleClass('hidden');
+  });
+
+});
