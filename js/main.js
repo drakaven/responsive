05a98e5adbe333ed243b60ac4e285c0ca35793c5
Drakaven <KnowNothingGames@gmail.com>
2017-03-15 12:59:17 -0400

    missing info JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
 $(document).ready(function () {
 
-  $('#form-submit').on('click', function (event) {
-    if ($('#telephone-input')[0].value === '') {
-      event.preventDefault();
-      //add missing notice
-    }
-  });
+    $('#form-submit').on('click', function (event) {
+        event.preventDefault();
+        var telephoneInput = $('#telephone-input');
+        if (telephoneInput[0].value === '') {
+            event.preventDefault();
+            if (!telephoneInput.next().hasClass('missing-info-div')) {
+                var div = $('<div />', {
+                    class: 'missing-info-div'
+                });
+                var img = $('<img />', {
+                    src: './Resources/error_icn.png',
+                    alt: 'error icon',
+                    class: 'missing-info'
+                });
+                var text = $('<span />', {
+                    html: 'Missing Information',
+                    class: 'missing-info-text'
+                });
+                div.append(img, text);
+                div.insertAfter(telephoneInput);
+                telephoneInput.toggleClass('missing-info-input');
+                telephoneInput.prevUntil('label').prev().toggleClass('missing-info-label');
+            }
+        }
+        else if (telephoneInput.next().hasClass('missing-info-div')) {
+            telephoneInput.next().remove();
+            telephoneInput.toggleClass('missing-info-input');
+            telephoneInput.prevUntil('label').prev().toggleClass('missing-info-label');
+        }
+    });
 
-  $('#business-card-checkbox').on('change', function () {
-    $('#business-card-section').toggleClass('hidden');
-  });
-
+    $('#business-card-checkbox').on('change', function () {
+        $('#business-card-section').toggleClass('hidden');
+    });
 });
